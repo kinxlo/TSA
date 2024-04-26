c5bc7fac9556d91c9fd161ebfc80bc0aed81dce1
Kingsley Solomon <kinxly@gmail.com>
2024-04-26 12:39:41 +0100

    init tsa with proper config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,14 @@
 // "use client"
 
+import { Ui } from '@tsa/ui';
+
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
   return (
-    <div>
-      <h1 className={`text-center text-blue-500 text-3xl`}>TSA WEB APP</h1>
+    <div className={`h-screen`}>
+      <h1 className={`text-center text-blue-500 text-3xl font-bold`}>
+        TSA WEB APP
+      </h1>
+      <Ui />
     </div>
   );
 }
---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable-next-line */
+export interface UiProps {}
+
+export function Ui(props: UiProps) {
+  return (
+    <div>
+      <h1 className={`text-red-500`}>
+        Welcome to Ui! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Quo cumque minima quos laudantium aspernatur ratione illum alias. Sequi
+        non laudantium, culpa cupiditate rerum nemo sunt fugit asperiores,
+        quibusdam voluptatibus necessitatibus.
+      </h1>
+    </div>
+  );
+}
+
+export default Ui;
